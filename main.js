947ef587ceec54d014f3fccc46f2df588264ab56
NyarlAzathoth <92152241+NyarlAzathoth@users.noreply.github.com>
2024-02-05 23:40:30 +0100

    Add (change number of squares) button
    The button redraws the drawing window after removing the last

--- main.js
+++ main.js
@@ -1,43 +1,67 @@
-let drawerSize = 16;
-let drawerSizePx = 960;
-let squareSizePx = drawerSizePx/drawerSize;
+let numSquares = 16;
+let drawingWindowSize = 960;
 
-let container = document.createElement('div');
-container = document.body.appendChild(container);
+function buildDrawingWindow (drawingWindowSize, numSquares) {
+    const pastContainer = document.querySelector('.drawingWindow');
 
-let squares = new Array(drawerSize);
-
-container.style.cssText=`
-    background-color: white;
-    height: ${drawerSizePx}px;width: ${drawerSizePx}px;
-    margin: 0;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    -ms-transform: translate(-50%, -50%);
-    transform: translate(-50%, -50%);
-    display: flex;
-    flex-wrap: wrap;
-`
-for (let i = 0; i<drawerSize; i++) {
-    squares[i] = new Array(drawerSize);
-    for (let j = 0; j<drawerSize; j++) {
-        let div = squares[i][j];
-        
-        div = document.createElement('div');
-        div.addEventListener('mouseover', function () {mouseOver(div)});
-        squares[i][j] = container.appendChild(div);
-        div = squares[i][j];
+    if (pastContainer !== null) {pastContainer.remove()};
     
-        div.style.cssText =`
-            height: ${squareSizePx}px;
-            width: ${squareSizePx}px;
-            flex: 0 0 auto;
-        `
+    if (numSquares > 100) {numSquares = 100};
+    let squareSize = drawingWindowSize/numSquares;
+
+    let container = document.createElement('div');
+    container.classList.add('drawingWindow');
+
+    container.style.cssText=`
+        background-color: white;
+        height: ${drawingWindowSize}px;width: ${drawingWindowSize}px;
+        margin: 0;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        -ms-transform: translate(-50%, -50%);
+        transform: translate(-50%, -50%);
+        display: flex;
+        flex-wrap: wrap;
+    `
+
+    let squares = new Array(numSquares);
+
+    for (let i = 0; i<numSquares; i++) {
+        squares[i] = new Array(numSquares);
+        for (let j = 0; j<numSquares; j++) {
+            let div = squares[i][j];
+            
+            div = document.createElement('div');
+            div.addEventListener('mouseover', function () {mouseOver(div)});
+            squares[i][j] = container.appendChild(div);
+            div = squares[i][j];
+        
+            div.style.cssText =`
+                height: ${squareSize}px;
+                width: ${squareSize}px;
+                flex: 0 0 auto;
+            `
+        }
+        
     }
+
+    container = document.body.appendChild(container);
 }
 
 function mouseOver (div) {
     div.style.backgroundColor = 'black';
 }
+
+function changeNumSquares () {
+    numSquares = prompt('How many squares ? (max:100)', '16')
+    buildDrawingWindow(drawingWindowSize, numSquares);
+}
+
+buildDrawingWindow(drawingWindowSize, numSquares);
+
+const sizeButton = document.querySelector('#size');
+
+sizeButton.addEventListener('click', function () {changeNumSquares()});
+
 
